465a4b8e7e11e43396cdc178685c721a2d74aa91
NakayaKana <kanakaya@me.com>
2016-06-10 03:16:15 +0900

    initial commit

---
+++ ListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ListTableViewCell.swift
+//  WordList
+//
+//  Created by NakayaKana on 2016/06/10.
+//  Copyright © 2016年 NakayaKana. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet var englishLabel: UILabel!
+    @IBOutlet var japaneseLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
